d50ae78a8c938b9fb1f119eac6f039aef84864a4
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-18 07:09:20 +0500

    ALLOWING THE SUMMARY PATH API

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from 'express'
 import { Port } from './Config.js'
 import cors from 'cors'
 import CodeRouter from './DB/Routes/CodeRoute.js'
+import SummaryRouter from './DB/Routes/SummaryRoute.js'
 
 const App = express()
 
 App.use(express.json())
 App.use(cors())
 App.use('/api/Code', CodeRouter)
+App.use('/api/Summary', SummaryRouter)
 App.listen(Port, () => {
   console.log(`RUNNING ON ${Port}`)
 })
