6723f27634cebafe06ae6064e3598e6d93703884
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-18 21:13:22 +0500

    removing both of the prompts and importing them from seperate files to make the code look clean

--- DB/Controllers/ApiCode.js
+++ DB/Controllers/ApiCode.js
@@ -2,6 +2,8 @@ import { doc, setDoc } from 'firebase/firestore'
 import { v4 as uuid } from 'uuid'
 import { db } from '../../Firebase.js'
 import { chatSessions } from '../../GemniConfig.js'
+import { CodePrompt } from '../Prompts/Code.js'
+import { ExplainPrompt } from '../Prompts/Explain.js'
 
 export const CodeController = async (req, res) => {
   const RandomID = uuid() // Unique ID for the code and explanation document
@@ -25,30 +27,16 @@ export const CodeController = async (req, res) => {
       }
     )
 
-    // Define the prompt for generating code
-    const CodePrompt = `
-    You are a highly experienced programmer with extensive knowledge in various programming languages. 
-    The user has provided the following task: "${codetask}". 
-    Your task is to generate a professional and correctly formatted code snippet that fulfills this requirement. 
-    Please provide only the code as your response. Do not include any additional text or explanations. 
-    Make sure the code is well-structured and follows best practices.`
-
     // Generate the code using Gemini AI
-    const Gemni_Response = await chatSessions.sendMessage(CodePrompt)
+    const Gemni_Response = await chatSessions.sendMessage(CodePrompt(codetask))
     const CodeAiResponse = Gemni_Response.response.text()
 
     // Check if the code response is valid
     if (CodeAiResponse) {
-      // Define the prompt for generating explanation
-      const ExplainPrompt = `
-      You have provided the following code for the task: "${codetask}":
-      ${CodeAiResponse}
-      Please provide a detailed explanation of how the code works. 
-      Explain each part of the code clearly and why it is implemented in that way. 
-      The explanation should help users understand the logic and functionality of the code.`
-
       // Generate the explanation using Gemini AI
-      const Gemni_Response2 = await chatSessions.sendMessage(ExplainPrompt)
+      const Gemni_Response2 = await chatSessions.sendMessage(
+        ExplainPrompt(codetask, CodeAiResponse)
+      )
       const ExplainAiResponse = Gemni_Response2.response.text()
 
       // Check if the explanation response is valid
