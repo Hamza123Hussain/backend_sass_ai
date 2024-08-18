061071db6853f1db6c9113c17928fc6aae4ec360
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-18 18:30:40 +0500

    allowing api path for the music and movie suggest ai to work proeprly

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { Port } from './Config.js'
 import cors from 'cors'
 import CodeRouter from './DB/Routes/CodeRoute.js'
 import SummaryRouter from './DB/Routes/SummaryRoute.js'
+import MusicRouter from './DB/Routes/MusicSuggestRoute.js'
+import { MovieController } from './DB/Controllers/MovieSuggest.js'
 
 const App = express()
 
@@ -10,6 +12,8 @@ App.use(express.json())
 App.use(cors())
 App.use('/api/Code', CodeRouter)
 App.use('/api/Summary', SummaryRouter)
+App.use('/api/Music', MusicRouter)
+App.use('/api/Movie', MovieController)
 App.listen(Port, () => {
   console.log(`RUNNING ON ${Port}`)
 })
