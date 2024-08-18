551690c19409ded87b786bb5ec83b3edd02aac43
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-19 03:23:26 +0500

    allowing api of auth

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import MovieRouter from './DB/Routes/MovieSuggest.js'
 import HashTagRouter from './DB/Routes/HashTagRoute.js'
 import GrammarRouter from './DB/Routes/Grammar.js'
 import ConvoRouter from './DB/Routes/ConvoRouter.js'
+import AuthRouter from './DB/Routes/AuthRouter.js'
 
 const App = express()
 
@@ -20,6 +21,7 @@ App.use('/api/Movie', MovieRouter)
 App.use('/api/HashTag', HashTagRouter)
 App.use('/api/Grammar', GrammarRouter)
 App.use('/api/General', ConvoRouter)
+App.use('/api/Auth', AuthRouter)
 App.listen(Port, () => {
   console.log(`RUNNING ON ${Port}`)
 })
