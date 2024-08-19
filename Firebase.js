a2a7e59c2b867137f5e54ec24f58564acf9d5fae
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-19 16:18:46 +0500

    fixed the firebase config file

--- Firebase.js
+++ Firebase.js
@@ -1,29 +1,24 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 import {
   apiKey,
   appId,
+  projectId,
   authDomain,
   messagingSenderId,
   storageBucket,
 } from './Config.js'
-import { getAuth } from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: authDomain,
-  projectId: 'chatgpt-clone-8a3be',
-  storageBucket: storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appId,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const Auth = getAuth(app)
