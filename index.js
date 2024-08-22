35dc8065e76b15e9281a90f7f837c0b2b6cbe492
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-22 19:42:20 +0500

    ADDED CORS OPTIONS

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ import ConvoRouter from './DB/Routes/ConvoRouter.js'
 import AuthRouter from './DB/Routes/AuthRouter.js'
 
 const App = express()
+// Enable CORS for all origins
+const corsOptions = {
+  origin: true, // Allow all origins https://notes-app-node-next-9x72.vercel.app/
+  optionsSuccessStatus: 200, // For legacy browser support
+}
 
 App.use(express.json())
-App.use(cors())
+App.use(cors(corsOptions))
 App.use('/api/Code', CodeRouter)
 App.use('/api/Summary', SummaryRouter)
 App.use('/api/Music', MusicRouter)
